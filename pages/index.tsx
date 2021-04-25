4c71d28613482f4489f13aa5e00662b1ba2e8e26
Robin Malfait <malfait.robin@gmail.com>
2021-04-25 19:38:06 +0200

    setup Next.js

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Schedule</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      Hello!
+
+    </div>
+  )
+}
